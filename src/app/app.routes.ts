3d13ae7db386b925e569dcb9c88e141d0e5ca1eb
Fernando Carta <cartafernando@outlook.com>
2024-12-25 21:52:37 -0500

    Registro de pacientes, SIN FUNCIONALIDAD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegistroPacienteComponent } from './registro-paciente/registro-paciente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'registro-paciente', component: RegistroPacienteComponent, title: 'Registro pacientes' },
+    { path: '', redirectTo: 'registro-paciente', pathMatch: 'full' }
+
+];
---
+++ src/app/registro-paciente/registro-paciente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'registro-paciente',
+  imports: [],
+  templateUrl: './registro-paciente.component.html',
+  styleUrl: './registro-paciente.component.css'
+})
+export class RegistroPacienteComponent {
+
+}
