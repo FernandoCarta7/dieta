259034340762623f6e815ef3af6222e45730db3e
Fernando Carta <cartafernando@outlook.com>
2025-01-28 21:51:40 -0500

    Registro paciente - validaciones de campos (Faltan validaciones)

--- src/app/PACIENTE/regitro-paciente/regitro-paciente.component.ts
+++ src/app/PACIENTE/regitro-paciente/regitro-paciente.component.ts
@@ -14,11 +14,24 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './regitro-paciente.component.css',
 })
 export class RegitroPacienteComponent {
+  //DELCLARACIÓN DE VARIABLES
   usuario: Usuario = new Usuario();
   ultimoUsuario: Usuario = new Usuario();
   id: number;
   paciente: Paciente = new Paciente();
   contrasenaValidar: string;
+  peso: number;
+  altura: number;
+  primerNombre: String;
+  primerApellido: String;
+  segundoNombre: String;
+  segundoApellido: String;
+  antecedentesMedicos: String;
+  fechaNacimiento: Date;
+  genero: String;
+  bandera: boolean;
+
+  //CONSTRUCTOR DEL COMPONENTE
   constructor(
     private pacienteServicio: PacienteService,
     private usuarioServicio: UsuarioService,
@@ -27,6 +40,15 @@ export class RegitroPacienteComponent {
   ngOnInit() {
     this.usuario.email = null;
     this.usuario.contrasena = '';
+    this.peso = 0.0;
+    this.altura = 0.0;
+    this.primerNombre = null;
+    this.primerApellido = null;
+    this.segundoNombre = ' - ';
+    this.segundoApellido = ' - ';
+    this.antecedentesMedicos = null;
+    this.fechaNacimiento = null;
+    this.genero = null;
   }
 
   onSubmitPaciente() {
@@ -34,17 +56,29 @@ export class RegitroPacienteComponent {
   }
 
   guardarPaciente() {
+    this.paciente.primerNombre = this.primerNombre;
+    this.paciente.primerApellido = this.primerApellido;
+    this.paciente.segundoNombre = this.segundoNombre;
+    this.paciente.segundoApellido = this.segundoApellido;
+    this.paciente.antecedentesMedicos = this.antecedentesMedicos;
+    this.paciente.fechaNacimiento = this.fechaNacimiento;
+    this.paciente.peso = this.peso;
+    this.paciente.altura = this.altura;
+    this.paciente.genero = this.genero;
+
     this.usuarioServicio.getLastUser().subscribe({
       next: (datos) => {
         this.id = datos.id;
-        this.pacienteServicio.agregarPaciente(this.paciente, this.id).subscribe(
-          {
+        this.pacienteServicio
+          .agregarPaciente(this.paciente, this.id)
+          .subscribe({
             next: (datos) => {
               this.irListadoUsuarios();
             },
-            error: (error: any) => { console.log("NO es un error, ES una oportunidad de mejora") }
-          }
-        )
+            error: (error: any) => {
+              console.log('NO es un error, ES una oportunidad de mejora');
+            },
+          });
       },
     });
     this.pacienteServicio.agregarPaciente(this.paciente, this.id).subscribe({
@@ -58,5 +92,8 @@ export class RegitroPacienteComponent {
   }
   irListadoUsuarios() {
     this.router.navigate(['listar-usuarios']);
-  };
+  }
+  validarCampos(): void {
+    this.bandera = false;
+  }
 }
